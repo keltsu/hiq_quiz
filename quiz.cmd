5bd0c370d08aa9054be19056507824158b69ff75
keltsu <39259262+keltsu@users.noreply.github.com>
2018-12-21 09:08:36 +0200

    loader alignment

--- quiz.cmd
+++ quiz.cmd
@@ -1,6 +1,6 @@
 @echo off
 title HiQ Robocon Quiz 2D (c) HiQ/Keltsu 2019
-MODE 190,52
+MODE 178,55
 color 06
 robot -d web_pipotest/ --outputdir \hiq_pipo\temp\reports --variablefile \hiq_pipo\web_competition\99_common_data\common_data.py .
 pause
