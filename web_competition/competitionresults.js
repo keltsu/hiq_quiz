2485170c8102a9cd5c5e8f3282cdd5577ad49a05
keltsu <ari.keltamaki@gmail.com>
2019-01-08 12:16:33 +0200

    optimizations

--- web_competition/competitionresults.js
+++ web_competition/competitionresults.js
@@ -1,1 +1,1 @@
-var hscore = "__'keltsu12345''12.08'
+var hscore = "__'keltsu12345''12.08'__'werlk''6.16'__'Speedy gonzales''16.9'__'keltsu''4.59'__'keltsu''4.94'__'Jari''262.68'__'keltsu''6.89'__'keltsu''6.09'__'keltsu''1.88'__'keltsu''5.54'__'keltsu''4.71'__'keltsu''5.15'__'keltsu''5.53'
